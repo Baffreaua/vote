571c7576eb2315d43b99833b1f933fc97e17a665
BAFFREAU Amaury <amaury.baffreau@epitech.eu>
2024-10-01 20:11:01 +0200

    vote and winning propos

--- dApp/src/VotingSystem.sol
+++ dApp/src/VotingSystem.sol
@@ -7,7 +7,13 @@ contract VotingSystem {
         uint256 voteCount;
     }
 
+    struct Voter {
+        bool voted;
+        uint256 id;
+    }
+
     Proposal[] public proposals;
+    mapping(address => Voter) public voters;
 
     constructor(string[] memory proposalNames) {
         for (uint256 x = 0; x < proposalNames.length; x++) {
@@ -15,6 +21,25 @@ contract VotingSystem {
                 name: proposalNames[x],
                 voteCount: 0
             }));
+        }
+    }
+
+    function vote(uint256 proposalId) public {
+        require(!voters[msg.sender].voted, "You have already voted.");
+        require(proposalId < proposals.length, "Invalid proposal ID.");
+
+        voters[msg.sender].voted = true;
+        voters[msg.sender].id = proposalId;
+        proposals[proposalId].voteCount++;
+    }
+
+    function winningProposal() public view returns (uint256 winningProposalId) {
+        uint256 winningVoteCount = 0;
+        for (uint256 i = 0; i < proposals.length; i++) {
+            if (proposals[i].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[i].voteCount;
+                winningProposalId = i;
+            }
         }
     }
 }
